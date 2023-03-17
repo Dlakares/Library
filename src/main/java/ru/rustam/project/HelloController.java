1e82ee566e6c92860e26c2b034328a5e1c3843c5
Basimov Rustam <sombra1nf@yandex.ru>
2023-03-17 18:03:39 +0500

    add Controller, DispatcherServletInitializer, SpringConfig. Delete config and Application context .xml

---
+++ src/main/java/ru/rustam/project/HelloController.java
@@ -0,0 +1,14 @@
+package ru.rustam.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
